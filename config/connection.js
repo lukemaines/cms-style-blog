78cf42121ff5b7b92c6a7585124640c8f73d92eb
l.maines <lmmaines28@gmail.com>
2024-09-04 19:49:32 -0400

    updated database_url

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,25 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+if (process.env.DATABASE_URL) { 
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false 
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'postgres'
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
     }
   );
 }
