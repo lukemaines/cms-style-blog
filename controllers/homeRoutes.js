64ac9ecb45f7a175c291dd536569e3b6e17ccc3c
l.maines <lmmaines28@gmail.com>
2024-09-04 21:10:44 -0400

    amended homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,4 +18,36 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+router.get('/login', (req, res) => {
+
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login'); 
+});
+
+
+router.get('/dashboard', async (req, res) => {
+  try {
+
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      }
+    });
+
+    const posts = postData.map(post => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
