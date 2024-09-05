95b6c2ca9d42715ff7f478ceef695a370ee13531
l.maines <lmmaines28@gmail.com>
2024-09-04 20:41:01 -0400

    changes

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -18,29 +18,4 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/post/:id', async (req, res) => {
-    try {
-      const postData = await Post.findByPk(req.params.id, {
-        include: [
-          { model: User, attributes: ['username'] },
-          { model: Comment, include: { model: User, attributes: ['username'] } }
-        ]
-      });
-  
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-  
-      const post = postData.get({ plain: true });
-  
-      res.render('single-post', {
-        post,
-        logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
 module.exports = router;
