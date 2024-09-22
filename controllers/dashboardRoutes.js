353ba46df56c21637110117187347bd20abc5c60
l.maines <lmmaines28@gmail.com>
2024-09-22 10:18:29 -0400

    added my dashboardroutes to render the dashboard

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const { Post, User } = require("../models");
+
+router.get("/", async (req, res) => {
+  if (!req.session.logged_in) {
+    return res.redirect("/login");
+  }
+
+  try {
+    const userId = req.session.user_id;
+    const postData = await Post.findAll({
+      where: {
+        user_id: userId,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("dashboard", {
+      posts,
+      logged_in: true,
+      username: req.session.username,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/new", (req, res) => {
+  if (!req.session.logged_in) {
+    return res.redirect("/login");
+  }
+  res.render("new-post", { logged_in: true });
+});
+
+router.get("/edit/:id", async (req, res) => {
+  if (!req.session.logged_in) {
+    return res.redirect("/login");
+  }
+  try {
+    const postData = await Post.findByPk(req.params.id);
+    if (!postData) {
+      res.status(404).json({ message: "No post found" });
+      return;
+    }
+    const post = postData.get({ plain: true });
+    res.render("edit-post", {
+      post,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
