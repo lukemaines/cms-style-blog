bc232710371653075f3edc7c75d5e4659236f4b3
l.maines <lmmaines28@gmail.com>
2024-09-22 19:59:22 -0400

    updated title and content ids in the js to match the handlebars form

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,8 +1,8 @@
 const newPostHandler = async (event) => {
     event.preventDefault();
   
-    const title = document.querySelector('#post-title').value.trim();
-    const content = document.querySelector('#post-content').value.trim();
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
   
     if (title && content) {
       const response = await fetch('/api/posts', {
