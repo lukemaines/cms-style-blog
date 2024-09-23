811eba21da32d9a4d5433fbcf5bb8bdd9dc8827a
l.maines <lmmaines28@gmail.com>
2024-09-22 21:44:09 -0400

    troubleshooting led to discovering redundancies in my index.js. removing to resolve

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes'); 
-const userRoutes = require('./api/userRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/', userRoutes);
 
 module.exports = router;
